e8b3ff6f341923dae881678ff193d6ea987ac477
lucaargolo <lucaargolo@gmail.com>
2020-11-16 14:08:43 -0300

    Properly handle CropConfig multipliers. Fix ColorHelper#changeSaturation generating invalid colors (Closes #4)

--- src/main/java/io/github/lucaargolo/seasons/mixin/FertilizeableMixin.java
+++ src/main/java/io/github/lucaargolo/seasons/mixin/FertilizeableMixin.java
@@ -7,7 +7,6 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.Registry;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -17,37 +16,45 @@ import java.util.Random;
 @Mixin({CropBlock.class, CocoaBlock.class, StemBlock.class})
 public abstract class FertilizeableMixin extends Block implements Fertilizable {
 
+    private boolean seasons$shouldInject = true;
+
     public FertilizeableMixin(Settings settings) {
         super(settings);
     }
 
     @Inject(at = @At("HEAD"), method = "randomTick", cancellable = true)
     public void randomTickInject(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
-        if(FabricSeasons.MOD_CONFIG.isSeasonMessingCrops()) {
+        if(FabricSeasons.MOD_CONFIG.isSeasonMessingCrops() && seasons$shouldInject) {
             Identifier cropIdentifier = Registry.BLOCK.getId(state.getBlock());
             float multiplier = FabricSeasons.MOD_CONFIG.getSeasonCropMultiplier(cropIdentifier, FabricSeasons.getCurrentSeason(world));
-            float k = multiplier - 1f;
-            if(k > 0 && random.nextFloat() <= k) {
-                this.randomTick(state, world, pos, random);
+            while(multiplier > 0f) {
+                float rand = random.nextFloat();
+                if(multiplier >= rand) {
+                    seasons$shouldInject = false;
+                    this.randomTick(state, world, pos, random);
+                    multiplier -= rand;
+                }
             }
-            if(k < 0 && random.nextFloat() <= (k+1f)) {
-                ci.cancel();
-            }
+            seasons$shouldInject = true;
+            ci.cancel();
         }
     }
 
     @Inject(at = @At("HEAD"), method = "grow", cancellable = true)
     public void growInject(ServerWorld world, Random random, BlockPos pos, BlockState state, CallbackInfo ci) {
-        if(FabricSeasons.MOD_CONFIG.isSeasonMessingBonemeal()) {
+        if(FabricSeasons.MOD_CONFIG.isSeasonMessingBonemeal() && seasons$shouldInject) {
             Identifier cropIdentifier = Registry.BLOCK.getId(state.getBlock());
             float multiplier = FabricSeasons.MOD_CONFIG.getSeasonCropMultiplier(cropIdentifier, FabricSeasons.getCurrentSeason(world));
-            float k = multiplier - 1f;
-            if(k > 0 && random.nextFloat() <= k) {
-                this.grow(world, random, pos, state);
+            while(multiplier > 0f) {
+                float rand = random.nextFloat();
+                if(multiplier >= rand) {
+                    seasons$shouldInject = false;
+                    this.grow(world, random, pos, state);
+                    multiplier -= rand;
+                }
             }
-            if(k < 0 && random.nextFloat() <= (k+1f)) {
-                ci.cancel();
-            }
+            seasons$shouldInject = true;
+            ci.cancel();
         }
     }
 
--- src/main/java/io/github/lucaargolo/seasons/utils/ColorHelper.java
+++ src/main/java/io/github/lucaargolo/seasons/utils/ColorHelper.java
@@ -13,7 +13,16 @@ public class ColorHelper {
         float g = color.getGreen()/255f;
         float b = color.getBlue()/255f;
         float p = MathHelper.sqrt((r*r*0.299f)+(g*g*0.597f)+(b*b*0.114));
-        return new Color(p+(r-p)*s, p+(g-p)*s, p+(b-p)*s);
+        float newRed = p+(r-p)*s;
+        float newGreen = p+(g-p)*s;
+        float newBlue = p+(b-p)*s;
+        if(newRed < 0f) newRed = 0f;
+        if(newRed > 1f) newRed = 1f;
+        if(newGreen < 0f) newGreen = 0f;
+        if(newGreen > 1f) newGreen = 1f;
+        if(newBlue < 0f) newBlue = 0f;
+        if(newBlue > 1f) newBlue = 1f;
+        return new Color(newRed, newGreen, newBlue);
     }
 
     public static Color changeHueSatBri(Color color, float h, float s, float b) {
