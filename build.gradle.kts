7d4fa087d4668e20c720b70abb14de26ca219027
Yankaree <sngc03769@gmail.com>
2026-06-09 08:53:17 -0400

    again

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
 
 plugins {
     id("maven-publish")
-    id("net.fabricmc.fabric-loom") version "1.17"
+    id("net.fabricmc.fabric-loom") version "1.17.5"
     id("org.ajoberstar.grgit") version "5.0.0"
     id("idea")
 }
