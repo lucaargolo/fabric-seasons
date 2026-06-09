75b645187ec313cb66a3bd7c7661c66d20abdfaa
Yankaree <sngc03769@gmail.com>
2026-06-09 09:50:24 -0400

    again

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,11 +70,11 @@ repositories {
 dependencies {
     add("minecraft", "com.mojang:minecraft:${project["minecraft_version"]}")
 
-    add("modImplementation", "net.fabricmc:fabric-loader:${project["loader_version"]}")
-    add("modImplementation", "net.fabricmc.fabric-api:fabric-api:${project["fabric_version"]}")
+    add("implementation", "net.fabricmc:fabric-loader:${project["loader_version"]}")
+    add("implementation", "net.fabricmc.fabric-api:fabric-api:${project["fabric_version"]}")
 
-    add("modRuntimeOnly", "com.terraformersmc:modmenu:${project["modmenu_version"]}")
-    add("modRuntimeOnly", "mezz.jei:jei-26.1.2-fabric:${project["jei_version"]}")
+    add("runtimeOnly", "com.terraformersmc:modmenu:${project["modmenu_version"]}")
+    add("runtimeOnly", "mezz.jei:jei-26.1.2-fabric:${project["jei_version"]}")
 }
 
 configurations.all {
